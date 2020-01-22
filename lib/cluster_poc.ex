7fc59bfcb18a3d120ea263d2ff3e97f152cbd2b2
chance <chance.a.cyphers@gmail.com>
2020-01-21 19:45:35 -0500

    auto connect nodes with epmd strategy

--- lib/cluster_poc.ex
+++ lib/cluster_poc.ex
@@ -1,5 +1,16 @@
 defmodule ClusterPoc do
   def start(_type, _args) do
-    Stuff.start_link()
+    topologies = [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [hosts: [:"foo@127.0.0.1", :"bar@127.0.0.1"]],
+      ]
+    ]
+    children = [
+      {Cluster.Supervisor, [topologies, [name: ClusterPoc.ClusterSupervisor]]},
+      %{id: ClusterPoc.Stuff, start: {Stuff, :start_link, []}}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: ClusterPoc.Supervisor)
   end
 end
